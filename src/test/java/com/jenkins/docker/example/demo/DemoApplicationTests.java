0d133625a61d1d5f93d0d34889c4885a23975e9b
unknown <sivadevi.java@gmail.com>
2021-07-10 15:55:54 -0400

    Second Commit

--- src/test/java/com/jenkins/docker/example/demo/DemoApplicationTests.java
+++ src/test/java/com/jenkins/docker/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.jenkins.docker.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
-
+ 
+	public static Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
 	@Test
 	void contextLoads() {
+		
+		logger.info("(\"Test Execution\")");
+		assertEquals("true","true");
 	}
 
 }
